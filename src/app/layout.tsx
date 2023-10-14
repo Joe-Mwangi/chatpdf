960bf4e61edea097e4ff5e7af8cf67445f7f3030
Joe-Mwangi <joem99271@gmail.com>
2023-10-14 16:22:42 +0300

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 // import Providers from "@/components/Providers";
 import { cn, constructMetadata } from "@/lib/utils";
 import { Inter } from "next/font/google";
@@ -28,7 +28,7 @@ export default function RootLayout({
         )}
       >
         <Toaster />
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
       </body>
       {/* </Providers> */}
