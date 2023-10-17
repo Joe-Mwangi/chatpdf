50af540439fc77abbf521126352cd7119484312c
Joe-Mwangi <joem99271@gmail.com>
2023-10-17 04:08:17 +0300

    updates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,7 @@ export function absoluteUrl(path: string) {
 }
 
 export function constructMetadata({
-  title = "ChatDocs - the SaaS for students",
+  title = "ChatDocs",
   description = "ChatDocs is an open-source software to make chatting to your PDF files easy.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
