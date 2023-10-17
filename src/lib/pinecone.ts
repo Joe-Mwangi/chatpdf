7a7e74377804ab11ecbd5480bf0d13242e71629c
Joe-Mwangi <joem99271@gmail.com>
2023-10-17 14:37:07 +0300

    pine 3

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,10 +1,12 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
 
 export const getPineconeClient = async () => {
-  const pinecone = new Pinecone({
+  const client = new PineconeClient();
+
+  await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
     environment: "gcp-starter",
   });
 
-  return pinecone;
+  return client;
 };
